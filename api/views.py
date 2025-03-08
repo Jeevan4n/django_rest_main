ed183af3293133cf5e9544ffd261e1137ddb605d
Jeevan4n <jeevanskumar444@gmail.com>
2025-03-09 00:35:36 +0530

    second-commit

--- api/views.py
+++ api/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render,HttpResponse
-from django.http import JsonResponse
+from django.shortcuts import render, HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import  api_view
+from students.models import Student
+from .serializers import StudentSerializer
 
-# Create your views here.
+
+@api_view(['GET','POST'])
 def studentsview(request):
-    students={
-        'id':2,
-        'name':'chintu',
-        'age':44
-    }
-    return HttpResponse(students)
+    if(request.method=='GET'):
+        #get all the a from the student table
+        students=Student.objects.all()
+        serializer=StudentSerializer(students,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    elif request.method=='POST':
+         serializer=StudentSerializer(data=request.data)
+         if serializer.is_valid():
+             serializer.save()
+             return Response(serializer.data,status=status.HTTP_201_CREATED)
+         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+
+    
 def jeev(request):
     return HttpResponse("po ra hooka")
