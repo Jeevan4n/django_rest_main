ac950b7c8fe0fa5479b7a01fc8bb55f2ed9cc44b
Jeevan4n <jeevanskumar444@gmail.com>
2025-03-09 10:59:38 +0530

    modified on api app

--- api/views.py
+++ api/views.py
@@ -1,27 +1,43 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import  api_view
+from rest_framework.decorators import api_view
 from students.models import Student
 from .serializers import StudentSerializer
 
 
-@api_view(['GET','POST'])
+@api_view(['GET', 'POST'])
 def studentsview(request):
+    if request.method == 'GET':
+        # Fetch all students
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def student_detail(request,pk):
+    #handles get,put,delete for a single student
+    student=get_object_or_404(Student,pk=pk)
     if(request.method=='GET'):
-        #get all the a from the student table
-        students=Student.objects.all()
-        serializer=StudentSerializer(students,many=True)
+        serializer=StudentSerializer(student)
         return Response(serializer.data,status=status.HTTP_200_OK)
-    
-    elif request.method=='POST':
-         serializer=StudentSerializer(data=request.data)
-         if serializer.is_valid():
-             serializer.save()
-             return Response(serializer.data,status=status.HTTP_201_CREATED)
-         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif(request.method=='POST'):
+        serializer=StudentSerializer(student)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+    elif(request.method=='DELETE'):
+        student.delete()
+        return Response({"message":"student deleted successfully"},status=status.HTTP_204_NO_CONTENT)
 
-
-    
 def jeev(request):
-    return HttpResponse("po ra hooka")
+    return HttpResponse("hello world")
+    
+
